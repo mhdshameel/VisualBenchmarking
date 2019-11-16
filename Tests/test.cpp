2c4dc5b74b036779ff5eb22020bfee74dbd36ca0
Anmol Jain <itsmeaj20@gmail.com>
2019-11-17 00:38:44 +0530

    Streamlined the project

---
+++ Tests/test.cpp
@@ -0,0 +1,21 @@
+#include "../Instrumentor.h"
+#include <iostream>
+
+int fibonacci(int number) {
+    PROFILE_FUNCTION();
+
+    if (number == 1) 
+        return 1;
+    if (number == 0)
+        return 0;
+    
+    return fibonacci(number - 1) + fibonacci(number - 2);
+}
+
+int main() {
+    int number;
+    std::cin >> number;
+    Instrumentor::Get().BeginSession("Session Name");
+    std::cout << fibonacci(number) << std::endl;
+    Instrumentor::Get().EndSession();
+}
